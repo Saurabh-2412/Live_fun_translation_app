401759b41890a17fc809161ea1c42dd125036d5b
saurabh <sharmasaurabh2412@gmail.com>
2020-11-30 16:40:54 +0530

    feat : added error handler functionality

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 const inputText = document.querySelector('#input-text');
-const outputText = document.querySelector('#output-text')
+const outputText = document.querySelector('#output-text');
+const btnTranslate = document.querySelector('#btn-translate')
 const serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json?"
 
 function getTranslationURL(inputText) {
     return serverURL + "text=" + inputText.value;
 }
- 
 
-document.querySelector('#btn-translate').addEventListener('click', () => {
+function ErrorHandler(error) {
+    console.log("Error occured " + error)
+}
+
+function clickEventHandler() {
     /* const response = fetch(`https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json?text=${inputText.value}`) */
-    
     fetch(getTranslationURL(inputText))
     .then(response => response.json())
     .then(json => outputText.innerHTML = json.contents.translated + " " + json.contents.text + " " + json.contents.translation)
-    .catch(error => console.log("Error occured : " + error))
-    
-})
+    .catch(ErrorHandler)
+    /* .catch(error => console.log("Error occured : " + error)) */
+}
+
+btnTranslate.addEventListener('click',clickEventHandler);
