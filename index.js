4d37f325503a8e596816796f0f2723e1c974f72b
saurabh <sharmasaurabh2412@gmail.com>
2020-12-13 11:55:53 +0530

    fix : changed api and fixed styling

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const inputText = document.querySelector('#input-text');
 const outputText = document.querySelector('#output-text');
-const btnTranslate = document.querySelector('#btn-translate')
-const serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json?"
+const btnTranslate = document.querySelector('#btn-translate');
+const serverURL = "https://api.funtranslations.com/translate/pirate.json?"
 
 function getTranslationURL(inputText) {
     return serverURL + "text=" + inputText.value;
@@ -15,7 +15,8 @@ function clickEventHandler() {
     /* const response = fetch(`https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json?text=${inputText.value}`) */
     fetch(getTranslationURL(inputText))
     .then(response => response.json())
-    .then(json => outputText.innerHTML = json.contents.translated + " " + json.contents.text + " " + json.contents.translation)
+    .then(json => outputText.innerHTML = json.contents.translated)
+    /* .then(json => outputText.innerHTML = json.contents.translated + " " + json.contents.text + " " + json.contents.translation) */
     .catch(ErrorHandler)
     /* .catch(error => console.log("Error occured : " + error)) */
 }
